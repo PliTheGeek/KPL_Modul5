97f3dd957c7a980b1f62319d7ce20d83a792ec0f
Dhafin Kamil <dhafin.kamil@gmail.com>
2025-03-13 11:27:54 +0700

    Add: Merge From branch

--- modul5_22111104018/Program.cs
+++ modul5_22111104018/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class Penjumlahan
 {
@@ -11,6 +12,32 @@ public class Penjumlahan
     }
 }
 
+public class SimpleDataBase<T>
+{
+    public List<T> storedData { get; private set; }
+    public List<DateTime> inputDates { get; private set; }
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.UtcNow);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine($"Data {i + 1} berisi: {storedData[i]}, yang disimpan pada waktu UTC: {inputDates[i]}");
+        }
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
@@ -18,5 +45,11 @@ class Program
         Penjumlahan penjumlahan = new Penjumlahan();
         int hasil = penjumlahan.JumlahTigaAngka(22, 11, 18);
         Console.WriteLine($"Hasil penjumlahan: {hasil}");
+
+        SimpleDataBase<int> dataBase = new SimpleDataBase<int>();
+        dataBase.AddNewData(22);
+        dataBase.AddNewData(11);
+        dataBase.AddNewData(18);
+        dataBase.PrintAllData();
     }
 }
--- modul5_22111104018/obj/Debug/net8.0/modul5_22111104018.AssemblyInfo.cs
+++ modul5_22111104018/obj/Debug/net8.0/modul5_22111104018.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul5_22111104018")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c873258df04ee5d83834f03ef3c0ac5233d8cf55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89859819160e6dcda186563050831efc3f39212a")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul5_22111104018")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul5_22111104018")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
