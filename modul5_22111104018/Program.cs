89859819160e6dcda186563050831efc3f39212a
Dhafin Kamil <dhafin.kamil@gmail.com>
2025-03-13 11:01:03 +0700

    Add project Baru

---
+++ modul5_22111104018/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Penjumlahan
+{
+    public T JumlahTigaAngka<T>(T angka1, T angka2, T angka3)
+    {
+        dynamic temp1 = angka1;
+        dynamic temp2 = angka2;
+        dynamic temp3 = angka3;
+        return temp1 + temp2 + temp3;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Penjumlahan penjumlahan = new Penjumlahan();
+        int hasil = penjumlahan.JumlahTigaAngka(22, 11, 18);
+        Console.WriteLine($"Hasil penjumlahan: {hasil}");
+    }
+}
